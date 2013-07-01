1140caccd910b573862af57b3bbf9e00f7d8f4b0
Patrick McAndrew <patrick@peerj.com>
2013-07-01 11:25:38 +0100

    Add missing parameter

--- DependencyInjection/DpnXmlSitemapExtension.php
+++ DependencyInjection/DpnXmlSitemapExtension.php
@@ -25,6 +25,7 @@ class DpnXmlSitemapExtension extends Extension
         $container->setParameter('dpn_xml_sitemap.defaults', $config['defaults']);
         $container->setParameter('dpn_xml_sitemap.http_cache', $config['http_cache']);
         $container->setParameter('dpn_xml_sitemap.max_per_sitemap', $config['max_per_sitemap']);
+        $container->setParameter('dpn_xml_sitemap.check_format', $config['check_format']);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
